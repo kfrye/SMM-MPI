8cef0f87049d1fedfeef65fbb93bff24938d2388
Kris Frye <kfrye@pdx.edu>
2015-08-25 15:45:06 -0700

    Added query for standard deviation scripts

---
+++ utilities/results/query-bt-dev.py
@@ -0,0 +1,38 @@
+import sqlite3
+import sys
+import statistics
+from decimal import *
+
+if len(sys.argv) != 2:
+  print("Syntax is: query-bt.py <# procs per node>")
+  exit(1)
+procs = sys.argv[1]
+
+conn = sqlite3.connect('results.db')
+classes = ["S", "W", "A", "B", "C"]
+proc_1 = [1, 4, 9, 16]
+proc_4 = [4, 9, 16, 25, 36, 49, 64] 
+if procs == "1":
+  processes = proc_1
+else:
+  processes = proc_4
+ 
+curs = conn.cursor()
+
+print('#\t', end="")
+for c in classes:
+  print(c, end="\t")
+
+for p in processes:
+  print(str('\n'+str(p)), end="\t")
+  for c in classes:
+    select_str = 'SELECT TIME_IN_SECONDS FROM bt WHERE PROC_PER_NODE=' + procs + ' AND TOTAL_PROCESSES=' + str(p) + ' AND CLASS="' + c + '" AND VERIFICATION="SUCCESSFUL"'
+    curs.execute(select_str)
+    data = curs.fetchall()
+    values = []
+    for val in data:
+      values.append(val[0])
+    if len(data) > 2:
+      print(round(statistics.stdev(values),2),end='\t')
+print('\n')
+conn.close()
---
+++ utilities/results/query-ft-dev.py
@@ -0,0 +1,37 @@
+import sqlite3
+import sys
+import statistics
+from decimal import *
+
+if len(sys.argv) != 2:
+  print("Syntax is: query-ft-dev.py <# procs per node>")
+  exit(1)
+
+procs = sys.argv[1]
+conn = sqlite3.connect('results.db')
+classes = ["S", "W", "A", "B", "C", "D", "E"]
+proc_1 = [1, 2, 4, 8, 16]
+proc_4 = [4, 8, 16, 32, 64]
+if procs == "1":
+  processes = proc_1
+else:
+  processes = proc_4
+
+curs = conn.cursor()
+
+print('#\t', end="")
+for c in classes:
+  print(c, end="\t")
+
+for p in processes:
+  print(str('\n'+str(p)), end="\t")
+  for c in classes:
+    curs.execute('SELECT TIME_IN_SECONDS FROM FT WHERE PROC_PER_NODE=' + procs + ' AND TOTAL_PROCESSES=' + str(p) + ' AND CLASS="' + c + '" AND VERIFICATION="SUCCESSFUL"')
+    data = curs.fetchall()
+    values = [] 
+    for val in data:
+      values.append(val[0]) 
+    if len(data) > 2: 
+      print(round(statistics.stdev(values),2),end='\t')
+print('\n')
+conn.close()
